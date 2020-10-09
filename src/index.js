b029c4f81d747f801ae8778387d36b85ec6fc2ba
Aliaksandr Lytsin <alexgooner13@gmail.com>
2020-10-10 02:42:51 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const config = new Map(bracketsConfig);
+    let stack = [];
+    let arrayOfBrackets = str.split('');
+    for (let item of arrayOfBrackets)
+        if (config.get(stack[stack.length - 1]) === item)
+            stack.pop();
+        else stack.push(item);           
+
+    return stack.length === 0;
 }
